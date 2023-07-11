2114f4949c9f0ffa1019a1dcd0eb5a4bde054eb5
anamikabhattacherjee <anamikabhattacharya63@gmail.com>
2023-07-11 12:06:49 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import "./App.css";
+import ReactPaginationComponents from "./reactpagination";
+import Basictable from "./table";
+
+function App() {
+  return <Basictable />;
+}
+
+export default App;
